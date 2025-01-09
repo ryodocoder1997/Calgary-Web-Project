53a50fc995c05d3019c871e4a74d752dbcf64c10
DinhAnh2301 <dinhanh2301@gmail.com>
2025-01-09 05:53:13 -0700

    feat: news page added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,50 +1,51 @@
 // Dinh Anh: Footer component
 
+import Link from 'next/link'
 import React from 'react'
 
 export default function NavBar() {
   return (
     <div className='mt-40 flex justify-between border border-secondary-500 pt-3 pb-3 pr-8 pl-8 bg-secondary-100 text-zinc-700 rounded-tl-none rounded-tr-[3rem] rounded-bl-[3rem] rounded-br-none'>
       <div className='flex flex-col pt-4 pb-4 pl-3'>
-        <text className='text-2xl font-bold leading-tight text-foreground'>
+        <p className='text-2xl font-bold leading-tight text-foreground'>
           Young Viet
-        </text>
-        <text className='text-2xl font-bold text-primary-600 mb-16'>@YYC</text>
-        <text>Contact Info:</text>
-        <text>Info@YoungvietYYC.com</text>
+        </p>
+        <p className='text-2xl font-bold text-primary-600 mb-16'>@YYC</p>
+        <p>Contact Info:</p>
+        <p>Info@YoungvietYYC.com</p>
       </div>
       <div className='pt-5 pr-3'>
         <div className='grid grid-cols-3 gap-x-9 gap-y-3 text-secondary-700'>
-          <a href='#' className='text-sm'>
+          <Link href='#' className='text-sm'>
             News
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             Volunteer
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             Instagram
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             Clubs
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             FAQs
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             Facabook
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             Mentor
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             About Us
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             Youtube
-          </a>
-          <a href='#' className='text-sm'>
+          </Link>
+          <Link href='#' className='text-sm'>
             Events
-          </a>
+          </Link>
         </div>
       </div>
     </div>
---
+++ src/app/news/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '@/app/components/NavBar'
+import Footer from '@/app/components/Footer'
+
+export default function News() {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  )
+}
