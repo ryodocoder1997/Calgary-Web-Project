f05a6493b52548b3da165c149d56e6186e56dcda
DinhAnh2301 <dinhanh2301@gmail.com>
2025-06-26 20:28:43 -0600

    feat: main element added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className='flex flex-col min-h-screen'>
       <NavBar />
-      <main className='my-5 text-black text-center flex-1'>Test</main>
+      <main className='my-5 text-black text-center flex-1'>MAIN CONTENT</main>
       <Footer />
     </div>
   )
